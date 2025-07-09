6bc319edd439ba863592fa5c01f8e4867fc93262
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-07-09 18:58:11 -0400

    Inicio parcial + creacion de ViajeApi

--- app/src/main/java/com/example/marcosduran_p2_ap2/data/remote/ViajeApi.kt
+++ app/src/main/java/com/example/marcosduran_p2_ap2/data/remote/ViajeApi.kt
@@ -1,2 +1,29 @@
-package com.example.marcosduran_p2_ap2.data.remote
+import com.example.marcosduran_p2_ap2.data.remote.dto.ViajeDto
+import okhttp3.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ViajeApi {
+    @GET("api/Viajes")
+    @Headers("X-API-Key: test")
+    suspend fun getViajes():List<ViajeDto>
+
+    @PUT("api/Viajes/{id}")
+    @Headers("X-API-Key: test")
+    suspend fun getViajesById(@Path("id") id: Int): Response<ViajeDto>
+
+    @POST("api/Viajes")
+    @Headers("X-API-Key: test")
+    suspend fun postViaje(@Body viajes: ViajeDto): ViajeDto
+
+    @DELETE("api/Viajes/{id}")
+    @Headers("X-API-Key: test")
+    suspend fun deleteViaje(@Path("id") id: Int?): Response<Unit>
+
+}
 
