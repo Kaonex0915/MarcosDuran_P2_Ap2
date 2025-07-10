3426dd1c3f3aca1f89efa4e06a4f34f53df50c5f
Marcos Duran <Marcos_Duran@ucne.edu.do>
2025-07-09 21:19:35 -0400

    Creando el Main activity

---
+++ app/src/main/java/com/example/marcosduran_p2_ap2/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.marcosduran_p2_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.example.marcosduran_p2_ap2.presentation.navigation.ViajeNavHost
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ViajeApiTheme {
+                val navHost = rememberNavController()
+                ViajeNavHost(navHost)
+            }
+        }
+    }
+}
